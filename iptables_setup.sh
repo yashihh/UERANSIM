5c316760a1e4666031eff18ac772d6b3f8f2aa29
yashihh <tseng890131@gmail.com>
2024-02-19 20:31:59 +0800

    tidy up

--- iptables_setup.sh
+++ iptables_setup.sh
@@ -7,6 +7,7 @@ sudo iptables -t mangle -L | grep TEE
 # namespace TSN2
 echo "**** namespace TSN2"
 sudo ip netns exec TSN iptables -t mangle -F PREROUTING #flush
+sudo ip netns exec TSN ip route add 10.60.0.0/24 dev uesimtun0
 sudo ip netns exec TSN iptables -t mangle -A PREROUTING -i enp0s10 -s  172.168.56.10 -j TEE --gateway 10.60.0.0
 sudo ip netns exec TSN iptables -t mangle -A PREROUTING -i uesimtun0 -s  172.168.56.3 -j TEE --gateway 172.168.56.0
 sudo ip netns exec TSN iptables -t mangle -L | grep TEE
